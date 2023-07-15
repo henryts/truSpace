c1b5f3edb992b388cd234251384fc35e83736d6b
henry sathyan <henrysathyan@gmail.com>
2023-07-15 12:32:40 +0530

    initial commit

---
+++ backend/routes/post.js
@@ -0,0 +1,11 @@
+import express from 'express';
+//import {getPost,createPost} from '../controllers/post.js'
+
+import  *  as controller from '../controllers/post.js';
+
+
+const router = express.Router();
+
+router.get('/',controller.getPost);
+router.post('/post',controller.createPost);
+export default router;
